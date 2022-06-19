e25484b140e24e03538f09b9c43e77154c8bba99
Alexandre <alexandre.ward@ecole-hexagone.com>
2022-06-20 00:19:50 +0200

    add button to scene

--- Assets/script/HandlePressButton.cs
+++ Assets/script/HandlePressButton.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test : MonoBehaviour
+public class HandlePressButton : MonoBehaviour
 {
+
+    public GameObject toShow;
+    public GameObject[] toHide;
     // Start is called before the first frame update
     void Start()
     {
